044080be671380ec5232c0ee384be51362bf6e02
Tiago Royer <royertiago@gmail.com>
2015-05-13 22:30:17 -0300

    enigma/types.hpp: Report uselessness of the typedefs.

--- enigma/types.hpp
+++ enigma/types.hpp
@@ -10,6 +10,8 @@ namespace enigma {
     /* Integer type used in the permutations.
      *
      * All the indexes represent values between 0 and 25.
+     *
+     * Testing with GCC showed a 4% slowdown changing signed char to int.
      */
     typedef signed char index;
 
