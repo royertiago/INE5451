ea09d567710561254128de82fb38dee46ce28622
Tiago Royer <royertiago@gmail.com>
2015-06-18 00:49:02 -0300

    aes/polynomial.h: Add constructor.

--- aes/polynomial.h
+++ aes/polynomial.h
@@ -7,13 +7,30 @@
 namespace aes {
     struct polynomial {
         unsigned char data;
+
+        polynomial() :
+            data(0)
+        {}
+
+        /* Construct a polynomial with the given bits set.
+         *
+         * Design decision: there will not be any implicit conversion functions
+         * between aes::polynomial and unsigned char.
+         * The arithmetic operators in aes::polynomial
+         * are very different from the unsigned char ones,
+         * so its better to have the compiler warn for erroneous operations
+         * than to discover them after hours of bug-hunting.
+         */
+        explicit polynomial( unsigned char data ) :
+            data(data)
+        {}
     };
 
     inline polynomial operator+( polynomial p, polynomial q ) {
-        return {(unsigned char)(p.data ^ q.data)};
+        return polynomial( p.data ^ q.data );
     }
     inline polynomial operator-( polynomial p, polynomial q ) {
-        return {(unsigned char)(p.data ^ q.data)};
+        return polynomial( p.data ^ q.data );
     }
     inline bool operator==( polynomial p, polynomial q ) {
         return p.data == q.data;
