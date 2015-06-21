25122a70360cd8f2d230966f122837e299868b84
Tiago Royer <royertiago@gmail.com>
2015-06-21 15:48:43 -0300

    Split test cases in polynomial.test.cpp.

--- test/polynomial.test.cpp
+++ test/polynomial.test.cpp
@@ -1,15 +1,15 @@
 #include "aes/polynomial.h"
 #include <catch.hpp>
 
-TEST_CASE( "aes::polynomial multiplication", "[aes]" ) {
-    using aes::polynomial;
+using aes::polynomial;
+
+TEST_CASE( "aes::polynomial multiplication", "[aes][polynomial]" ) {
     CHECK( (polynomial(0xcb) * polynomial(0x02)) == polynomial(0x8d) );
     CHECK( (polynomial(0x57) * polynomial(0x13)) == polynomial(0xfe) );
     CHECK( (polynomial(0x53) * polynomial(0xca)) == polynomial(0x01) );
 }
 
-TEST_CASE( "aes::polynomial::inv", "[aes]" ) {
-    using aes::polynomial;
+TEST_CASE( "aes::polynomial::inv", "[aes][polynomial]" ) {
     for( int i = 1; i < 256; i++ ) {
         polynomial p(i);
         polynomial inv = p.inv();
@@ -18,8 +18,7 @@ TEST_CASE( "aes::polynomial::inv", "[aes]" ) {
     }
 }
 
-TEST_CASE( "aes::polynomial division", "[aes]" ) {
-    using aes::polynomial;
+TEST_CASE( "aes::polynomial division", "[aes][polynomial]" ) {
     for( int i = 0; i < 256; i++ )
         for( int j = 1; j < 256; j++ ) {
             polynomial p(i);
@@ -29,11 +28,9 @@ TEST_CASE( "aes::polynomial division", "[aes]" ) {
         }
 }
 
-TEST_CASE( "aes::polynomial bit accessors", "[aes]" ) {
-    using aes::polynomial;
+TEST_CASE( "aes::polynomial bit accessor methods", "[aes][polynomial]" ) {
     polynomial p(0x71);
 
-    // Operation with accessor methods
     CHECK( p.get(0) == true );
     CHECK( p.get(1) == false );
     CHECK( p.get(2) == false );
@@ -54,8 +51,10 @@ TEST_CASE( "aes::polynomial bit accessors", "[aes]" ) {
     CHECK( p.get(7) == false );
 
     CHECK( p == polynomial(0x71) );
+}
 
-    // Operation with polynomial::bit_accessor and boolean literals
+TEST_CASE( "aes::polynomial::operator[] with boolean literals", "[aes][polynomial]" ) {
+    polynomial p(0x71);
     CHECK( p[0] == true );
     CHECK( p[1] == false );
     CHECK( p[2] == false );
@@ -74,8 +73,10 @@ TEST_CASE( "aes::polynomial bit accessors", "[aes]" ) {
     CHECK( p[1] == false );
     p[7] = false;
     CHECK( p[7] == false );
+}
 
-    // Operations between bit_accessor's.
+TEST_CASE( "aes::polynomial::operator[] with other operator[]", "[aes][polynomial]" ) {
+    polynomial p(0x71);
     CHECK( p[1] == p[2] );
     CHECK( p[4] == p[5] );
     CHECK( p[1] != p[4] );
@@ -108,7 +109,10 @@ TEST_CASE( "aes::polynomial bit accessors", "[aes]" ) {
     CHECK( p[5] == false );
     p[5] = ~p[5];
     CHECK( p[5] == true );
+}
 
+TEST_CASE( "Exceptions in aes::polynomial bit accessors", "[aes][polynomial]" ) {
+    polynomial p;
     // Exception tests
     CHECK_THROWS_AS( p.get(8), std::out_of_range );
     CHECK_THROWS_AS( p.set(8), std::out_of_range );
