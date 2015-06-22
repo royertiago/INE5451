4b2a3f09341c904c38f1735865713246ca0187ab
Tiago Royer <royertiago@gmail.com>
2015-06-22 02:44:13 -0300

    WIP: add support for literate programming.

--- literate.mk
+++ literate.mk
@@ -11,3 +11,25 @@ $(literatepdf): %.pdf: %.tex
 
 $(literatetex): %.tex: %.nw
 	noweave -latex -delay $< > $@
+
+define make-noweb-rule
+
+$(warning command is noroots $1 | sed 's/^<<\(.*\)>>$$/\1/')
+$(eval roots := $(shell noroots $1 | sed 's/^<<\(.*\)>>$$/\1/'))
+$(warning roots is $(roots))
+$(eval roots := $(patsubst %,$(dir $1)%,$(roots))))
+
+noweb : $(roots)
+
+$(warning roots is $(roots))
+$(warning 1 is $1)
+
+$(foreach file,$(roots),$(eval $(file) : $1
+	notangle -R$(notdir $@) $1 > $@))
+
+$(warning done.)
+endef
+
+noweb:
+
+$(foreach file, $(literatesrc),$(eval$(call make-noweb-rule,$(file))))
