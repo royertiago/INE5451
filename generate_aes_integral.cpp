637d791c47b77deef4ceb693867feace285af882
Tiago Royer <royertiago@gmail.com>
2015-06-23 21:06:36 -0300

    Add generate_aes_integral.cpp.

---
+++ generate_aes_integral.cpp
@@ -0,0 +1,35 @@
+/* This program generates integrals for the Integral Attack for AES.
+ *
+ * Input format:
+ *  <32 hexadecimal digits representing the constants of the integral constants>
+ *  <32 hexadecimal digits representing the key>
+ *  <index of the saturated byte>
+ *  <number of rounds>
+ * This format may be repeated an arbitrary number of times.
+ * Each integral will be separated by an empty line.
+ */
+
+#include <iostream>
+#include "aes/aes.h"
+
+int main() {
+    aes::matrix key, integral;
+    int index, i, j, rounds;
+
+    const char * sep = "";
+
+    while( std::cin >> integral >> key >> index >> rounds ) {
+        std::cout << sep;
+        sep = "\n";
+
+        i = index % 4;
+        j = index / 4;
+
+        for( int k = 0; k < 256; k++ ) {
+            integral[i][j] = aes::polynomial(k);
+            std::cout << aes::aes(integral, key, rounds) << '\n';
+        }
+    }
+
+    return 0;
+}
