b5147d2657ff8efb2597658bae26601adec761d7
Tiago Royer <royertiago@gmail.com>
2015-06-30 16:19:23 -0300

    Implement math::extended_euclid recursively.

--- math/algo.hpp
+++ math/algo.hpp
@@ -20,6 +20,22 @@ namespace math {
         return r;
     }
 
+    /* Data for the Extended Euclid's algorithm.
+     */
+    template< typename T >
+    struct euclid_data {
+        T x;
+        T y;
+        T gcd; // The greatest common divisor
+    };
+
+    template< typename T >
+    euclid_data<T> extended_euclid( const T& a, const T& b ) {
+        if( b == T(0) )
+            return {1, 0, a};
+        euclid_data<T> data = extended_euclid( b, a % b );
+        return {data.y, data.x - T(a/b) * data.y, data.gcd};
+    }
 }
 
 #endif // MATH_ALGO_HPP
--- test/math.test.cpp
+++ test/math.test.cpp
@@ -8,3 +8,21 @@ TEST_CASE( "Square and Multiply algorithm", "[math]" ) {
     CHECK( math::pow(5, 3) == 125 );
     CHECK( math::pow(7, 0) == 1 );
 }
+
+TEST_CASE( "Extended Euclid", "[math]" ) {
+    math::euclid_data<int> data;
+
+    data = math::extended_euclid( 3, 5 );
+    CHECK( 1 == data.gcd );
+    CHECK( 1 == data.x * 3 + data.y * 5 );
+    data = math::extended_euclid( 5, 3 );
+    CHECK( 1 == data.gcd );
+    CHECK( 1 == data.x * 5 + data.y * 3 );
+
+    data = math::extended_euclid( 32, 24 );
+    CHECK( 8 == data.gcd );
+    CHECK( 8 == data.x * 32 + data.y * 24 );
+    data = math::extended_euclid( 24, 32 );
+    CHECK( 8 == data.gcd );
+    CHECK( 8 == data.x * 24 + data.y * 32 );
+}
