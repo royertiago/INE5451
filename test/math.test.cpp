9834523b0ca2284fd4001821529a5eb5d0e5dc48
Tiago Royer <royertiago@gmail.com>
2015-06-30 22:33:02 -0300

    math.test.cpp: Harden tests for extended_euclid.

--- test/math.test.cpp
+++ test/math.test.cpp
@@ -9,20 +9,31 @@ TEST_CASE( "Square and Multiply algorithm", "[math]" ) {
     CHECK( math::pow(7, 0) == 1 );
 }
 
+void test_extended_euclid( int a, int b ) {
+    math::euclid_data<int> data = math::extended_euclid( a, b );
+    int xabs = data.x > 0 ? data.x : -data.x;
+    int yabs = data.y > 0 ? data.y : -data.y;
+    for( int x = -xabs + 1; x < xabs; x++ )
+        for( int y = -yabs + 1; y < yabs; y++ ) {
+            CHECK( 0 == (a*x + b*y) % data.gcd );
+            if( a*x + b*y > 0 )
+                CHECK( (a*x + b*y) > data.gcd );
+            else if( a*x + b*y < 0 )
+                CHECK( (a*x + b*y) < -data.gcd );
+            else {
+                CHECK( (x == 0 || a == 0) );
+                CHECK( (y == 0 || b == 0) );
+            }
+        }
+}
+
 TEST_CASE( "Extended Euclid", "[math]" ) {
     math::euclid_data<int> data;
 
-    data = math::extended_euclid( 3, 5 );
-    CHECK( 1 == data.gcd );
-    CHECK( 1 == data.x * 3 + data.y * 5 );
-    data = math::extended_euclid( 5, 3 );
-    CHECK( 1 == data.gcd );
-    CHECK( 1 == data.x * 5 + data.y * 3 );
-
-    data = math::extended_euclid( 32, 24 );
-    CHECK( 8 == data.gcd );
-    CHECK( 8 == data.x * 32 + data.y * 24 );
-    data = math::extended_euclid( 24, 32 );
-    CHECK( 8 == data.gcd );
-    CHECK( 8 == data.x * 24 + data.y * 32 );
+    test_extended_euclid( 3, 5 );
+    test_extended_euclid( 5, 3 );
+    test_extended_euclid( 32, 24 );
+    test_extended_euclid( 24, 32 );
+    test_extended_euclid( 0, 5 );
+    test_extended_euclid( 5, 0 );
 }
